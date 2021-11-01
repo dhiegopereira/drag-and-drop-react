0b62b2bee2d682b7ef6c74df29af18539de2193d
Diego Pereira <diego@develop.diego.develop.com.br>
2021-11-01 09:22:32 -0300

    first commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { useDrag } from 'react-dnd'
+
+/**
+ * Your Component
+ */
+export default function Card({ isDragging, text }) {
+  const [{ opacity }, dragRef] = useDrag(
+    () => ({
+      type: 'CARD',
+      item: { text },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.5 : 1
+      })
+    }),
+    []
+  )
+  return (
+    <div ref={dragRef} style={{ opacity }}>
+      {text}
+    </div>
+  )
+}
